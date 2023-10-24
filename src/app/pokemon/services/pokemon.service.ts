61b8566de6a79fc8ebec6f4b870de843d9cbd15b
Adrián Fonseca Vega <afonsecav88@gmail.com>
2023-10-23 22:04:04 -0300

    service pokemon with handled error implemented

--- src/app/pokemon/services/pokemon.service.ts
+++ src/app/pokemon/services/pokemon.service.ts
@@ -1,11 +1,13 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { apiUrl } from '../../../environments/environments.prod';
 import { HttpClient } from '@angular/common/http';
+import { Pokemon } from '../intefaces/pokemon.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
+  public pokemons = signal<Pokemon[]>([]);
   public apiUrl = apiUrl;
   public http = inject(HttpClient);
 
@@ -13,8 +15,16 @@ export class PokemonService {
 
   getPokemon(pokemonName: string) {
     const url: string = `${this.apiUrl}/${pokemonName}`;
-    this.http.get(url).subscribe((data) => {
-      console.log('Desde el servicio', data);
+    this.http.get<Pokemon[]>(url).subscribe({
+      next: (pokemon: Pokemon[]) => {
+        this.pokemons.set(pokemon);
+        console.log(pokemon);
+      },
+      error: (error) => {
+        {
+          console.log('Ha ocurrido un error', error);
+        }
+      },
     });
   }
 }
