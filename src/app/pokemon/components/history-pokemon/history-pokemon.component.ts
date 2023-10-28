8df5e683ab59238253ba8196b6d86030613216b7
Adrián Fonseca Vega <afonsecav88@gmail.com>
2023-10-27 23:06:11 -0300

    show list of search pokemon in sidenav

--- src/app/pokemon/components/history-pokemon/history-pokemon.component.ts
+++ src/app/pokemon/components/history-pokemon/history-pokemon.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Pokemon } from '../../intefaces/pokemon.interface';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-history-pokemon',
   templateUrl: './history-pokemon.component.html',
   styleUrls: ['./history-pokemon.component.css'],
 })
-export class HistoryPokemonComponent {
-  public historySearch: string[] = ['hola'];
+export class HistoryPokemonComponent implements OnInit {
+  public historySearchPokemons!: Pokemon[];
+  private pokemonService = inject(PokemonService);
+
+  ngOnInit(): void {
+    this.historySearchPokemons = this.pokemonService.pokemon;
+  }
 
   searchPokemon() {
     console.log('has buscado un pokemon');
